584f4f9c42919c0b9248efe57a75c4ec68c3716b
Todd Brannon <toddqbrannon@gmail.com>
2023-06-16 21:38:44 -0500

    make cards light background and dark text

--- components/LandingSection4.jsx
+++ components/LandingSection4.jsx
@@ -3,9 +3,9 @@ import Image from 'next/image';
 const LandingSection4 = () => {
   return (
     <div className="grid lg:grid-cols-3 gap-8 mb-20 mx-4 sm:mx-8 md:mx-12 lg:mx-16 xl:mx-20">
-        <div className="card border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+        <div className="card border border-gray-200 rounded-lg p-8 md:p-12">
             <div className="card-body">
-              <h2 className="card-head cyan_gradient dark:cyan_gradient">Start a Conversation</h2>
+              <h2 className="card-head text-blue-900">Start a Conversation</h2>
               <p className="card-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod dignissim nisl, ac consectetur 
                 dolor placerat a. In eu nulla nec orci vulputate varius.</p>
               <div className="card-actions justify-end">
@@ -13,9 +13,9 @@ const LandingSection4 = () => {
               </div>
             </div>
         </div>
-        <div className="card border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+        <div className="card border border-gray-20 rounded-lg p-8 md:p-12">
             <div className="card-body">
-              <h2 className="card-head cyan_gradient dark:cyan_gradient">Boost Your Listing</h2>
+              <h2 className="card-head text-blue-900">Boost Your Listing</h2>
               <p className="card-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod dignissim nisl, ac consectetur 
                 dolor placerat a. In eu nulla nec orci vulputate varius.</p>
               <div className="card-actions justify-end">
@@ -23,9 +23,9 @@ const LandingSection4 = () => {
               </div>
             </div>
         </div>
-        <div className="card border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+        <div className="card border border-gray-200 rounded-lg p-8 md:p-12">
             <div className="card-body">
-              <h2 className="card-head cyan_gradient dark:cyan_gradient">Partner with Others</h2>
+              <h2 className="card-head text-blue-900">Partner with Others</h2>
               <p className="card-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod dignissim nisl, ac consectetur 
                 dolor placerat a. In eu nulla nec orci vulputate varius.</p>
               <div className="card-actions">
